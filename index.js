4d6b6d25ebc431017d4a5ac186239fc4c2068ce8
ojiiis <official.ojingirisamuel@gmail.com>
2025-11-22 10:27:31 +0100

    s

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require("express");
 const  {ojs} = require("ojs-loader");
 
 
-const autoRes = (req,res) =>{
-    
-}
+const allNigeriaStates = [
+  "Benue", "FCT Abuja", "Kogi", "Kwara", "Nasarawa", "Niger", "Plateau",
+  "Adamawa", "Bauchi", "Borno", "Gombe", "Taraba", "Yobe",
+  "Kaduna", "Kano", "Katsina", "Kebbi", "Sokoto", "Jigawa", "Zamfara",
+  "Abia", "Anambra", "Ebonyi", "Enugu", "Imo",
+  "Akwa Ibom", "Bayelsa", "Cross River", "Delta", "Edo", "Rivers",
+  "Ekiti", "Lagos", "Ogun", "Ondo", "Osun", "Oyo"
+];
 const app = express();
 
 /*   get routes  */
 app.get("/",(req,res)=>{
-    res.end(ojs.get('web/index.html'));
+    res.end(ojs.get('web/index.html',{location:"Nigeria"}));
 });
 
 app.get("/sample",(req,res)=>{
